a2023054e47feb2fee89041de694a41470693e2d
jls83 <josephlsanders@gmail.com>
2022-08-29 01:26:20 -0400

    Add duplicate examples for the Arena-based solution

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use crate::arena_trie::ArenaTrie;
-// use crate::trie::Trie;
+use crate::trie::Trie;
 
 pub mod arena_trie;
-// pub mod trie;
+pub mod trie;
 
 fn main() {
-    let mut trie = ArenaTrie::new();
+    let mut trie = Trie::new();
 
     // Insert a few values with a weight; higher values show higher in the results.
     trie.insert_with_score("Foo".to_string(), 0);
@@ -33,6 +33,38 @@ fn main() {
     }
 
     match trie.search("Banana".to_string()) {
+        Some(found_word) => println!("Found {}", found_word),
+        _ => println!("Did not find word"),
+    }
+    // All of the above examples also work with an Arena-based solution!
+    let mut arena_trie = ArenaTrie::new();
+
+    // Insert a few values with a weight; higher values show higher in the results.
+    arena_trie.insert_with_score("Foo".to_string(), 0);
+    arena_trie.insert_with_score("For".to_string(), 8);
+    arena_trie.insert_with_score("Foreign".to_string(), 10);
+
+    // Add some values that won't match just to be sure :)
+    arena_trie.insert_with_score("Bar".to_string(), 0);
+    arena_trie.insert_with_score("Baz".to_string(), 0);
+
+    // Should print values in the expected order ("Foreign", "For", "Foo").
+    for result in arena_trie.get_ranked_results("Fo".to_string()).unwrap().iter() {
+        println!("{}", result);
+    }
+
+    // Testing for membership works as expected as well.
+    match arena_trie.search("For".to_string()) {
+        Some(found_word) => println!("Found {}", found_word),
+        _ => println!("Did not find word"),
+    }
+
+    match arena_trie.search("Bar".to_string()) {
+        Some(found_word) => println!("Found {}", found_word),
+        _ => println!("Did not find word"),
+    }
+
+    match arena_trie.search("Banana".to_string()) {
         Some(found_word) => println!("Found {}", found_word),
         _ => println!("Did not find word"),
     }
